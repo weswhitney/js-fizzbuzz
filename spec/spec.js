487010453958f37b54577f96f6124567490d921f
weswhitney <wesleywhitney@gmail.com>
2015-06-03 19:13:00 -0600

    added spec.js file and added a spec

---
+++ spec/spec.js
@@ -0,0 +1,10 @@
+
+var yourCode = require('../src');
+
+describe("fizzBuzz", function () {
+
+  it("returns the number when provided 1 as input", function () {
+    expect(yourCode.fizzBuzz(1)).toEqual("1");
+  });
+  
+});
